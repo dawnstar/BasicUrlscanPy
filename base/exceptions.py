5c77ce195331dd611ef23a58cf575c2b45b5acb2
Daniel McNamara <dawnstar@dawnstar.systems>
2025-04-07 15:52:39 +1000

    Changes to how exec handles JSON means custom exception no longer needed

--- base/exceptions.py
+++
@@ -1,7 +0,0 @@
-class UrlscanInvalidPayload(Exception):
-    
-    """
-    A custom exception for invalid payloads in urlscan.io API requests. This may be extended in the future to include more specific error handling.
-    """
-    
-    pass
